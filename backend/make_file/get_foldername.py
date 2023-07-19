6814e6de1cce7b370216d3ed529a822480e204eb
winterflw <winterflw@hansung.ac.kr>
2023-07-19 14:37:29 +0000

    upgrade foldername

--- backend/make_file/get_foldername.py
+++ backend/make_file/get_foldername.py
@@ -1,14 +1,29 @@
+import pandas_market_calendars as mcal
 import pytz
 import calendar
 from datetime import datetime, timedelta
 
-
 def get_previous_business_day(date):
-    previous_day = date - timedelta(days=1)
-    while previous_day.weekday() >= 5:  # Weekdays are 0-4, weekends are 5-6
-        previous_day -= timedelta(days=1)
+    # Use New York Stock Exchange calendar
+    nyse = mcal.get_calendar('NYSE')
+    
+    # Convert the 'date' to New York time, as the NYSE calendar is based on New York time
+    date = date.astimezone(pytz.timezone('America/New_York'))
+
+    # Get all valid days from the start of 2000 till 'date'
+    valid_days = nyse.valid_days(start_date='2000-01-01', end_date=date.replace(tzinfo=None))  # remove timezone info
+    
+    # The last valid day is the previous business day
+    previous_day = valid_days[-1]
+
+    # Convert the 'previous_day' back to 'America/New_York' timezone
+    previous_day = previous_day.tz_convert(pytz.timezone('America/New_York')).to_pydatetime()
+
     return previous_day
 
+def is_business_day(date):
+    nyse = mcal.get_calendar('NYSE')  # Use New York Stock Exchange calendar
+    return not nyse.valid_days(start_date=date.replace(tzinfo=None), end_date=date.replace(tzinfo=None)).empty  # remove timezone info
 
 def week_of_month(date):
     first_day_of_month = date.replace(day=1)
@@ -16,9 +31,10 @@ def week_of_month(date):
     adjusted_dom = day_of_month + first_day_of_month.weekday()
     return int((adjusted_dom - 1) / 7) + 1
 
-
 def get_daily_data():
-    today = datetime.now()
+    today = datetime.now(pytz.timezone('America/New_York'))
+    if not is_business_day(today):  # Pass datetime object instead of string
+        return None  # If today is not a business day, return None
     fixedday = get_previous_business_day(today)
     composeday = get_previous_business_day(fixedday)
 
@@ -28,57 +44,36 @@ def get_daily_data():
 
 
 def get_weekly_data():
-    now = datetime.now(pytz.timezone('Asia/Seoul'))
-    today = now
-
-    # Find last Friday
-    if today.weekday() >= 4:  # If it's Friday or later
-        last_friday = today - timedelta(days=(today.weekday() - 4))
-    else:  # If it's before Friday
-        last_friday = today - timedelta(days=(today.weekday() + 3))
-
-    # Find last Monday
-    last_monday = last_friday - timedelta(days=4)
+    today = datetime.now(pytz.timezone('America/New_York'))
+    if not is_business_day(today.strftime("%Y-%m-%d")):
+        return None
+    last_friday = get_previous_business_day(today)
+    last_monday = get_previous_business_day(last_friday)
     week_of_month_now = week_of_month(last_friday)
     folder_name = last_friday.strftime(f"%Y/%Y-%m/weekly/{week_of_month_now}")
-
     report = 'W'
-
     return folder_name, last_friday, last_monday, report
 
-
 def get_monthly_data():
-    now = datetime.now(pytz.timezone('Asia/Seoul'))
-    today = now
-    year = now.year
-    month = now.month
-
-    # Subtract one month
+    today = datetime.now(pytz.timezone('America/New_York'))
+    if not is_business_day(today.strftime("%Y-%m-%d")):
+        return None
+    year = today.year
+    month = today.month
     if month == 1:
         month = 12
         year -= 1
     else:
         month -= 1
-
-    # Get the first day of last month
-    first_day_of_last_month = datetime(year, month, 1)
-
-    # Get the first business day of last month
+    first_day_of_last_month = datetime(year, month, 1, tzinfo=pytz.timezone('America/New_York'))
     first_business_day = first_day_of_last_month
-    while first_business_day.weekday() >= 5:
+    while not is_business_day(first_business_day.strftime("%Y-%m-%d")):
         first_business_day += timedelta(days=1)
-
-    # Get the last day of last month
-    last_day_of_last_month = calendar.monthrange(year, month)
-    last_day_of_last_month = datetime(year, month, last_day_of_last_month)
-
-    # Get the last business day of last month
+    last_day_of_last_month = calendar.monthrange(year, month)[1]
+    last_day_of_last_month = datetime(year, month, last_day_of_last_month, tzinfo=pytz.timezone('America/New_York'))
     last_business_day = last_day_of_last_month
-    while last_business_day.weekday() >= 5:
+    while not is_business_day(last_business_day.strftime("%Y-%m-%d")):
         last_business_day -= timedelta(days=1)
-
     folder_name = first_day_of_last_month.strftime("%Y/%Y-%m")
-
     report = 'M'
-
     return folder_name, last_business_day, first_business_day, report
